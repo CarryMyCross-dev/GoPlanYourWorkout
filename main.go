a541ea83e71cf47e871a63bb439822c5095f0aa0
CarryMyCross-dev <cyberthreet@gmail.com>
2025-08-11 15:49:24 -0500

    wip

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
- package main
+package main
 
- import (
- 	"fmt"
- )
+import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
+)
 
- func main () {
- 	fmt.Print("new project")
- }
+func main() {
+	a := app.New()
+	w := a.NewWindow("Hello World")
+
+	w.SetContent(widget.NewLabel("Hello World!"))
+	w.ShowAndRun()
+}
